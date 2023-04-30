aee68b5cea7b8707cff9960e05a13105d096ac84
Emmanuel Oladipupo <emmanuel.oladipupo@decagon.dev>
2023-04-30 06:48:16 +0100

    add global exception handler

---
+++ src/main/java/com/logikcode/fileupload/exception/GlobalException.java
@@ -0,0 +1,15 @@
+package com.logikcode.fileupload.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalException {
+    @ExceptionHandler(TooManyFilesException.class)
+    public ResponseEntity<String> handleTooManyFilesException(TooManyFilesException tooManyFilesException){
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(tooManyFilesException.getMessage());
+    }
+}
