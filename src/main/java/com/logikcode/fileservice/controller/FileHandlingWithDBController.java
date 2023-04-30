69ab4e1c73ca7ca987bee92d8bc1d1bd779a92f9
Emmanuel Oladipupo <emmanuel.oladipupo@decagon.dev>
2023-04-30 07:58:42 +0100

    add file entity,file repository, controller for persisting to db

---
+++ src/main/java/com/logikcode/fileservice/controller/FileHandlingWithDBController.java
@@ -0,0 +1,11 @@
+package com.logikcode.fileservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/api/v1/file")
+public class FileHandlingWithDBController {
+
+
+}
